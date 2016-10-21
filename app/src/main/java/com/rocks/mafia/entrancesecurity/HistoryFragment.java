686caab9d47fbdefeb12ab0c3d88f69740d1d5df
tarun <tarun14110@iiitd.ac.in>
2016-10-21 15:06:13 +0530

    removed image from history page due to slow loading

--- app/src/main/java/com/rocks/mafia/entrancesecurity/HistoryFragment.java
+++ app/src/main/java/com/rocks/mafia/entrancesecurity/HistoryFragment.java
@@ -1,6 +1,7 @@
 package com.rocks.mafia.entrancesecurity;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.sql.Time;
 import java.util.ArrayList;
@@ -43,51 +46,14 @@ public class HistoryFragment extends ListFragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
-        HistoryHandler  handler= new HistoryHandler(getActivity());
+
         recyclerView = (RecyclerView) getView().findViewById(R.id.allHistory);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(llm);
-
-        demoData = handler.getAllHistory();
-        int i;
-        adapter = new HistoryArrayAdaptor(demoData);
-
-        recyclerView.setAdapter(adapter);
-
-
-/*
-    // TARUN DATA
-     String[] dummyData = new String[]{"Tarun", "Vibha", "Mukesh", "Chetna", "Shikha","Tarun", "Vibha", "Mukesh", "Chetna", "Shikha","Tarun", "Vibha", "Mukesh", "Chetna", "Shikha"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1,  dummyData);
-        setListAdapter(adapter);*/
-
-
-/*
-//PANKAJ DATA WORKING-part1
-
-HistoryHandler  handler= new HistoryHandler(getActivity());
-        historyData = new ArrayList<HistoryNode>();
-
-        String n= handler.getDatabaseName();
-        handler.addHistory(new HistoryNode("Sujeet", new Time(3,4,5)));
-        handler.addHistory(new HistoryNode("Tarun", new Time(3,4,5)));
-        handler.addHistory(new HistoryNode("Mukesh", new Time(3,4,5)));
-        handler.addHistory(new HistoryNode("Sudhir", new Time(3,4,5)));
-        handler.addHistory(new HistoryNode("pankaj", new Time(3,4,5)));
-        historyData= handler.getAllHistory();
-
-*/
-
-
-     /*
-       //PANKAJ DATA WORKING-part2
-       historyNodeArrayAdapter = new HistoryArrayAdaptor(getActivity(), historyData);
-        setListAdapter(historyNodeArrayAdapter);
-        handler.close();
-*/
+        FetchHistoryDataFromSQL fetchHistoryDataFromSQL = new FetchHistoryDataFromSQL();
+        fetchHistoryDataFromSQL.execute();
     }
 
     @Override
@@ -95,13 +61,24 @@ HistoryHandler  handler= new HistoryHandler(getActivity());
         super.onListItemClick(l, v, position, id);
     }
 
-/*
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.history_display, container, false);
+    public class FetchHistoryDataFromSQL extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            recyclerView.setAdapter(adapter);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HistoryHandler  handler= new HistoryHandler(getActivity());
+            // get all history data
+            demoData = handler.getAllHistory();
+            adapter = new HistoryArrayAdaptor(demoData);
+
+            return null;
+        }
     }
 
-*/
+
 }
