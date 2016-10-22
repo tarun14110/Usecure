e489e389480899387b9383438a670d4de3dc3356
tarun <tarun14110@iiitd.ac.in>
2016-10-22 06:09:00 +0530

    added functionality of sending notification from security end to user end

--- app/src/main/java/com/rocks/mafia/entrancesecurity/SecurityRequestFragment.java
+++ app/src/main/java/com/rocks/mafia/entrancesecurity/SecurityRequestFragment.java
@@ -8,6 +8,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -27,12 +28,19 @@ import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SecurityRequestFragment extends Fragment {
     private View view;
-
+    private static final String TAG = SecurityMainActivity.class.getSimpleName();
     private String title;//String for tab title
 
     private static RecyclerView recyclerView;
@@ -91,6 +99,7 @@ public class SecurityRequestFragment extends Fragment {
         final EditText name = (EditText) promptView.findViewById(R.id.editname);
         final EditText reason = (EditText) promptView.findViewById(R.id.editReason);
         final EditText time = (EditText) promptView.findViewById(R.id.editTime);
+        final EditText whomToContact = (EditText) promptView.findViewById(R.id.whomToContactText);
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
         alertDialogBuilder.setView(promptView);
@@ -104,6 +113,10 @@ public class SecurityRequestFragment extends Fragment {
                         String givenName=name.getText().toString();
                         String givenReason=reason.getText().toString();
                         String givenTime=time.getText().toString();
+                        String givenWhomToContact = whomToContact.getText().toString();
+                        Log.e("BHAI", givenWhomToContact);
+                        SendOutsiderdata sendOutsiderdata = new SendOutsiderdata(givenName, givenReason, givenTime, givenWhomToContact);
+                        sendOutsiderdata.execute();
                         dialog.cancel();
                     }
                 })
@@ -120,5 +133,113 @@ public class SecurityRequestFragment extends Fragment {
     }
 
 
+    public void sendOutsiderData(String name, String reason, String time, String whomToContact) {
+        // sending gcm token to server
+        // Create data variable for sent values to server
+        SessionManager session = new SessionManager(getActivity());
+        String data = null;
+        try {
+            data = URLEncoder.encode("contact", "UTF-8")
+                    + "=" + URLEncoder.encode(whomToContact, "UTF-8");
+            data += "&" + URLEncoder.encode("name", "UTF-8")
+                    + "=" + URLEncoder.encode(name, "UTF-8");
+            data += "&" + URLEncoder.encode("reason", "UTF-8")
+                    + "=" + URLEncoder.encode(reason, "UTF-8");
+            data += "&" + URLEncoder.encode("time", "UTF-8") + "="
+                    + URLEncoder.encode(time, "UTF-8");
+
+            Log.e("DATATATA", data);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+        String text = "";
+        BufferedReader reader=null;
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("http://usecure.site88.net/handleSecurityRequest.php");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+            Log.e(TAG, "Error while sending RegId data " + ex.toString());
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
+
+        // Show response on activity
+        // content.setText( text  );
+        if (text.contains("error")) {
+            Log.e(TAG, "Error while Sending request to the server" + text);
+        } else {
+            Log.e(TAG, "Succesfully Sent request to the server" + text);
+
+        }
+
+
+        Log.e(TAG, "sendRegistrationToServer: " + whomToContact + "  to contact " +session.getContact());
+    }
+
+    public class SendOutsiderdata extends AsyncTask<Void,Void,Void> {
+
+        String name, reason, time, whomToContact;
+
+        SendOutsiderdata(String name, String reason, String time, String whomToContact) {
+            this.name = name;
+            this.reason = reason;
+            this.time = time;
+            this.whomToContact = whomToContact;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // sending Outsider data to the server
+            sendOutsiderData(name, reason, time, whomToContact);
+            return null;
+        }
+    }
 
 }
